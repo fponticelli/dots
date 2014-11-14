9ce0384866c8f5de53a8d2cf6ca8126e5a806e6a
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-11-13 22:47:17 -0700

    improved Html.parse

--- src/dots/Html.hx
+++ src/dots/Html.hx
@@ -1,22 +1,33 @@
 package dots;
 
 using StringTools;
+import js.Browser;
 import js.html.Element;
 import js.html.Node;
 import js.html.NodeList;
 
 class Html {
+  static var pattern = ~/[<]([^> ]+)/;
   public static function parseNodes(html : String) : NodeList {
-    var el = js.Browser.document.createElement('div');
+    if(!pattern.match(html))
+      throw 'Invalid pattern "$html"';
+    var el = switch pattern.matched(1).toLowerCase() {
+      case "tbody",
+           "thead": Browser.document.createElement("table");
+      case "td",
+           "th": Browser.document.createElement("tr");
+      case "tr": Browser.document.createElement("tbody");
+      case _:    Browser.document.createElement("div");
+    };
     el.innerHTML = html;
     return el.childNodes;
   }
 
   public inline static function parseArray(html : String) : Array<Element>
-    return nodeListToArray(parseNodes(html.trim()));
+    return nodeListToArray(parseNodes(html));
 
   public inline static function parse(html : String) : Element
-    return cast parseNodes(html.ltrim())[0];
+    return cast parseNodes(html)[0];
 
   public inline static function nodeListToArray(list : NodeList) : Array<Element>
     return untyped __js__('Array.prototype.slice.call')(list, 0);
